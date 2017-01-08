cac2c4087af2ae4013c27aa79192c770c89c9b02
Nathan-HB <nhansonb@gmail.com>
2017-01-08 22:38:32 +0000

    ITS WORKING
    Added colour changes

--- js/colors.js
+++ js/colors.js
@@ -1,3 +1,6 @@
+/**
+ * Created by Nathan on 08-Jan-17.
+ */
 var swatch = document.getElementsByClassName('colorFrame');
 
 for (var i= 0, n=swatch.length;i<n;i++){
@@ -5,11 +8,11 @@ for (var i= 0, n=swatch.length;i<n;i++){
 }
 
 function setColor(color){
-    c.fillSytle = color;
+    c.fillStyle = color;
     c.strokeStyle = color;
-    var active = document.getElementsByClassName('colorFrame active')[0];
+    var active = document.getElementsByClassName('active')[0];
         if(active){
-            active.classname = 'colorFrame'
+            active.className = 'colorFrame'
         }
 }
 
@@ -17,7 +20,7 @@ function setPalette(e){
     //ID which colorFrame is being clicked
     var swatch = e.target;
     //Set the fill/strokeStyle to the background color of the colorFrame that has been clicked
-    setColor(colorFrame.style.backgroundColor);
+    setColor(swatch.style.backgroundColor);
     //Change the class of the colorFrame that has been clicked to colorFrame active
-    colorFrame.Classname += ' active'
+    swatch.className += ' active'
 }
